c07a489c6afadc6ec75487a91acf5b94d90a6934
inaciojr48 <54165291+inaciojr48@users.noreply.github.com>
2021-10-06 02:37:11 +0200

    screenshots

---
+++ app/src/main/java/com/decode/nextjob/ui/JobInfo.kt
@@ -0,0 +1,14 @@
+package com.decode.nextjob.ui
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.decode.nextjob.R
+
+class JobInfo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.info_job)
+
+    }
+}
