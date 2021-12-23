072d1f106e25054b95c73824b01cbf4bf2f55211
inaciosacataria <inaciosacataria@gmail.com>
2021-12-24 01:31:22 +0200

    update

---
+++ app/src/main/java/com/decode/nextjob/models/IndeedJob.kt
@@ -0,0 +1,14 @@
+package com.decode.nextjob.models
+
+class IndeedJobs () {
+}
+
+class Job(var job_title:String,
+    var url:String,
+    var company_name : String,
+    var job_location: String,
+    var is_remote: Boolean,
+    var job_salary:String,
+    var Job_description: String){
+
+}
